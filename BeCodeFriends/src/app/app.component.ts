1d717cca3f2bc7da17da7bd94bffdfff0e3ea806
Peter <dhondt.peter@gmail.com>
2021-04-16 16:39:06 +0200

    create get request function to display friend list

--- BeCodeFriends/src/app/app.component.ts
+++ BeCodeFriends/src/app/app.component.ts
@@ -1,27 +1,46 @@
-import { Component } from '@angular/core';
-import { Friend } from './friend';
-import { AddFriendService } from './add-friend.service';
+import {Component, OnInit} from '@angular/core';
+import {Friend} from './friend';
+import {AddFriendService} from './add-friend.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
   languages = ['JavaScript', 'PHP', 'Python', 'C#'];
 
   friendModel = new Friend('', '', '', '', '');
 
+  allFriends = [];
+
   constructor(private _addFriendService: AddFriendService) {
   }
 
-  onSubmit() {
+  ngOnInit(): any {
+    this.getAllFriends('http://localhost:9078/allFriends');
+    console.log(this.allFriends);
+  }
+
+  onSubmit(): void {
     this._addFriendService.addFriend(this.friendModel)
       .subscribe(
-        data => console.log('it worked', data),
+        success => this.getAllFriends('http://localhost:9078/allFriends'),
         error => console.error('it did not work', error)
-     );
+      );
+    console.log(this.allFriends);
+  }
+
+  public async getAllFriends(url: string): Promise<any> {
+    return await fetch(url, {
+      method: 'get',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(response => response.json())
+      .then(data => this.allFriends = data);
   }
 
 }
